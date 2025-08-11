4c6fa60af57ffe859ba7ff83c0fcf27fa77fb64a
Burn221 <nevmyvaka.n.05@gmail.com>
2025-08-11 20:23:24 +0600

    first commit

---
+++ src/main/java/ru/burn221/gymlite/GymliteApplication.java
@@ -0,0 +1,13 @@
+package ru.burn221.gymlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/burn221/gymlite/GymliteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.burn221.gymlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
